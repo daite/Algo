0b08f363518ddd1796db49992831ccbc62b7fdf2
daite <blueskykind02@yahoo.co.jp>
2024-03-13 21:34:12 +0900

    Update solution.py

--- 937. Reorder Data in Log Files/solution.py
+++ 937. Reorder Data in Log Files/solution.py
@@ -11,6 +11,9 @@
 # The digit-logs maintain their relative ordering.
 # Return the final order of the logs.
 
+# Input: logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+# Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+
 def reorderLogFiles(self, logs: List[str]) -> List[str]:
     letters = []
     digits = []
