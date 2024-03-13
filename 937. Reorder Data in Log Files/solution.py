0bd821071d4050116e5eea6a4ea7bd9d73fb7879
daite <blueskykind02@yahoo.co.jp>
2024-03-13 21:39:35 +0900

    updated

--- 937. Reorder Data in Log Files/solution.py
+++ 937. Reorder Data in Log Files/solution.py
@@ -14,7 +14,9 @@
 # Input: logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
 # Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
 
-def reorderLogFiles(self, logs: List[str]) -> List[str]:
+import unittest
+
+def reorderLogFiles(logs):
     letters = []
     digits = []
     for log in logs:
@@ -24,3 +26,33 @@ def reorderLogFiles(self, logs: List[str]) -> List[str]:
             letters.append(log)
     letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
     return letters + digits
+
+class TestReorderLogFiles(unittest.TestCase):
+
+    def test_reorderLogFiles(self):
+        logs = ["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"]
+        expected_output = ["g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"]
+        self.assertEqual(reorderLogFiles(logs), expected_output)
+
+    def test_empty_logs(self):
+        logs = []
+        expected_output = []
+        self.assertEqual(reorderLogFiles(logs), expected_output)
+
+    def test_all_digit_logs(self):
+        logs = ["1 2 3", "4 5 6", "7 8 9"]
+        expected_output = ["1 2 3", "4 5 6", "7 8 9"]
+        self.assertEqual(reorderLogFiles(logs), expected_output)
+
+    def test_all_letter_logs(self):
+        logs = ["abc def ghi", "jkl mno pqr", "stu vwx yz"]
+        expected_output = ["abc def ghi", "jkl mno pqr", "stu vwx yz"]
+        self.assertEqual(reorderLogFiles(logs), expected_output)
+
+    def test_mixed_logs_with_same_content(self):
+        logs = ["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo", "a8 act zoo"]
+        expected_output = ["g1 act car", "a8 act zoo", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"]
+        self.assertEqual(reorderLogFiles(logs), expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
